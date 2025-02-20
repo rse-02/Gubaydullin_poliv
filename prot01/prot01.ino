5cda892e0eefcb597cb4f95eebb22353dec5232e
Andrew2014rg <egundina223@gmail.com>
2025-02-20 19:18:53 +0300

    написали прототип кода

---
+++ prot01/prot01.ino
@@ -0,0 +1,24 @@
+
+
+
+//насос запуск
+void nasos_start(){
+    digitalWrite(2, HIGH);  // включаем помпу [1](https://kit.alexgyver.ru/tutorials/pump/)
+  }
+//насос стоп
+void nasos_stop (){
+
+}
+//измер влаж
+void vlazh (){
+
+}
+void setup (){
+   pinMode(2, OUTPUT);  // D2 — выход [1](https://kit.alexgyver.ru/tutorials/pump/)
+    
+}
+void loop(){
+ 
+
+}
+
--- sketch_jan9b.ino
+++
@@ -1,7 +0,0 @@
-void nasos_start(){
-  //насос запуск
-}
-
-void nasos_stop (){}
-остановить насос
-void vlazh ()}
