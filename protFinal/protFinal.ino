ee5fbf493b7b429c66df319004b7ee15e92247df
BALARA <egundina223@gmail.com>
2025-05-30 13:56:01 +0300

    23

--- protFinal/protFinal.ino
+++ protFinal/protFinal.ino
@@ -5,11 +5,11 @@ int v_data = 0; // Переменная для хранения значения
 
 //насос запуск
 void nasos_start(){
-    digitalWrite(2, 1);  // включаем помпу [1](https://kit.alexgyver.ru/tutorials/pump/)
+    digitalWrite(3, 1);  // включаем помпу [1](https://kit.alexgyver.ru/tutorials/pump/)
   }
 //насос стоп
 void nasos_stop (){
-  digitalWrite(2, 0);
+  digitalWrite(3, 0);
 }
 //измер влаж
 void vlazh (){
@@ -23,35 +23,46 @@ void vlazh (){
   // Задержка перед следующим измерением
   delay(1000); // 1 секунда
 }
+
+void nasos_test(){
+  nasos_start();
+  delay(1000);
+  nasos_stop();
+  delay(1000);
+
+}
 void setup (){
-   pinMode(2, OUTPUT);  // D2 — выход [1](https://kit.alexgyver.ru/tutorials/pump/)
+   pinMode(3, OUTPUT);  // D2 — выход [1](https://kit.alexgyver.ru/tutorials/pump/)
     int s=1;
 }
 void loop(){
+nasos_test();
+nasos_stop();
+delay(2000);
 //-----------------
-//Состояния
-  //Измерить
-  if (s==1){
-    Serial.println("Измеряем влажность");
-    vlazh();
-    Serial.print("Влажность ");
-    Serial.print(v_data);
-    if (v_data<100){
-       s=2;  
-    }
-  }
-  //полив
-  if (s==2){
-        Serial.println("Поливаю...");
-     nasos_start();
-      vlazh();
-       if (v_data>1000){
-          nasos_stop();
-          s=1;  
-       }
-  }
-  //Разряжен
-  if (s==3){
+// //Состояния
+//   //Измерить
+//   if (s==1){
+//     Serial.println("Измеряем влажность");
+//     vlazh();
+//     Serial.print("Влажность ");
+//     Serial.print(v_data);
+//     if (v_data<100){
+//        s=2;  
+//     }
+//   }
+//   //полив
+//   if (s==2){
+//         Serial.println("Поливаю...");
+//      nasos_start();
+//       vlazh();
+//        if (v_data>1000){
+//           nasos_stop();
+//           s=1;  
+//        }
+//   }
+//   //Разряжен
+//   if (s==3){
     
-  }
+//   }
 }
